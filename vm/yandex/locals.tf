e352611ca3d9623d3625891f0692b18966594274
PavelZ <pwlonw@gmail.com>
2025-11-30 22:45:27 +0300

    Fix Yandex VMs

--- vm/yandex/locals.tf
+++ vm/yandex/locals.tf
@@ -2,7 +2,7 @@ locals {
   size_params = {
     tiny   = { cores = 2, memory = 1, boot_disk_size = 20, platform = "standard-v4a", fraction = 50, boot_disk_type = "network-hdd" }
     small  = { cores = 2, memory = 2, boot_disk_size = 20, platform = "standard-v4a", fraction = 100, boot_disk_type = "network-hdd" }
-    medium = { cores = 4, memory = 6, boot_disk_size = 20, platform = "standard-v4a", fraction = 100, boot_disk_type = "network-ssd" }
+    medium = { cores = 4, memory = 4, boot_disk_size = 20, platform = "standard-v4a", fraction = 100, boot_disk_type = "network-ssd" }
     large  = { cores = 4, memory = 8, boot_disk_size = 20, platform = "standard-v4a", fraction = 100, boot_disk_type = "network-ssd" }
   }
 

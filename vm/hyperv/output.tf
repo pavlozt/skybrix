1b456baa991e1ff0d3857bc9818d83a054eab602
PavelZ <pwlonw@gmail.com>
2026-01-23 18:12:23 +0300

    Fix hyper-v naming

--- vm/hyperv/output.tf
+++ vm/hyperv/output.tf
@@ -1,7 +1,7 @@
 output "host_info" {
   description = "Host information for Ansible"
   value = {
-    name         = hyperv_machine_instance.vm.name
+    name         = var.name
     ip           = try(hyperv_machine_instance.vm.network_adaptors[0].ip_addresses[0], null)
     ssh_username = var.ssh_username
   }
